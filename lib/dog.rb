8576d4eb92043a9d5234c77e5e2230c4885aa902
michael otero <michaelotero1118@gmail.com>
2020-02-06 02:11:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 # Your code goes here!
-class Dog 
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
   
+  def bark
+    put "Woof!"
+  end
+end
+
